9426b005a1b9e32561c56206738ca31b4889a14a
Alexandre Magno de Lima Ferreira <alexmlf93@gmail.com>
2020-07-22 22:08:36 -0300

    Configurando navegação

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const SignUp: React.FC = () => {
+  const ex = '';
+  return (
+    <>
+      <Container />
+    </>
+  );
+};
+
+export default SignUp;
